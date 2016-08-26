b1994a41a3e1aeb1d9ba28144d6bb6516d5ce8aa
jerry <jerry.mo@fulaan.com>
2016-08-26 16:51:55 +0800

    嗨，大家好

---
+++ src/main/java/com/app/api/TextApi.java
@@ -0,0 +1,7 @@
+package com.app.api;
+
+/**
+ * Created by moslpc on 2016/8/26.
+ */
+public class TextApi {
+}
--- src/main/java/com/app/util/OkHttpUtil.java
+++ src/main/java/com/app/util/OkHttpUtil.java
@@ -1,7 +1,40 @@
 package com.app.util;
 
+import okhttp3.*;
+
+import java.io.IOException;
+
 /**
  * Created by mosl on 16/8/25.
  */
 public class OkHttpUtil {
+
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    public static final MediaType XML
+            = MediaType.parse("application/xml; charset=utf-8");
+
+    public static String run(String url) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public static String post(String url, String json) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
 }
