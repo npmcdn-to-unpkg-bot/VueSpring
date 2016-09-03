f25855559eed5ac09a6a973d9db79d2e8c8da96a
pzzls <moshenglei@icloud.com>
2016-09-03 13:39:33 +0800

    add

---
+++ src/main/java/com/app/exceptionresolver/AnnotationHandlerMethodExceptionResolver.java
@@ -0,0 +1,11 @@
+package com.app.exceptionresolver;
+
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+
+/**
+ * Created by mosl on 16/9/3.
+ */
+public class AnnotationHandlerMethodExceptionResolver extends ExceptionHandlerExceptionResolver {
+
+
+}
